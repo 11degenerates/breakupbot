5fb4ace990164a33b6d8a982b75057ee1c0968f3
11degenerates <robfleming100@gmail.com>
2025-09-21 12:04:54 -0500

    Create ping.tsx

---
+++ pages/ping.tsx
@@ -0,0 +1,9 @@
+// pages/ping.tsx
+export default function PingPage() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>Ping OK</h1>
+      <p>This page proves the Pages Router is being deployed.</p>
+    </main>
+  );
+}
